a742360ea873d94cace2fcb6116a6715e4f0e18b
Denise Godinez <119900012+dgodinez44@users.noreply.github.com>
2025-09-17 21:51:38 +0000

    undo

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,6 +1,5 @@
 
 #include <stdint.h>
-#include "rprintf.h"
 
 #define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
 
@@ -10,31 +9,6 @@ uint8_t inb (uint16_t _port) {
     uint8_t rv;
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
     return rv;
-}
-
-int putc(int data){
-    static int offset = 0; // Keep track of the current position
-    unsigned char *video_memory = (unsigned char *)0xB8000;
-
-    if (data == '\n'){
-        //Move to the next line (80 chars * 2 bytes = 160 bytes per line)
-        offset = ((offset / 160)) * 160;
-    }
-    else if (data == '\r') {
-        // Move to beginning of current line
-        offset = (offset /160) * 160;
-    } else {
-        video_memory[offset] = data;    // Write ASCII character
-        video_memory[offset +1] = 0x07;  // White on black attribute
-        offset += 2;                   // Move to the next position
-    }
-
-    // See if the end of the screen is reached
-    if (offset >= 160 * 25){
-        offset =0;
-    }
-
-    return data;
 }
 
 void main() {
@@ -49,6 +23,4 @@ void main() {
         }
     }
 
-    esp_printf(putc, "Current execution level: %d\n", 0);
-    esp_printf(putc, "Hello from terminal driver!\n");
 }
